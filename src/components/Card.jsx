12cac0d9a52ce7cbb507014ff8d0024971ec9680
Wellinton <wellintongerson@hotmail.com>
2023-07-19 16:43:01 -0300

    add:icones para redirecionamento

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,14 +1,24 @@
+import { VscVmRunning, VscGithubInverted } from "react-icons/vsc";
 import axios from "axios";
+
 import "../styles/card.css";
 
 export function Card(props) {
   return (
-    <div className="Card flex h-[350px] w-[300px] hover:w-[309px] transition-all m-auto justify-center bg-[#1A202C] relative  rounded-lg border-[1px] border-[#1A202C] overflow-hidden">
+    <div className="Card flex h-[370px] w-[300px] hover:w-[325px] transition-all m-auto justify-center bg-[#1A202C] relative  rounded-lg border-[1px] border-[#1A202C] overflow-hidden">
       <h1 className="text-white mt-5">{props.name.toUpperCase()}</h1>
-      <div className="Dialog flex justify-center text-white absolute h-[220px] w-[320px] bottom-0 bg-black/50 ">
+      <div className="Dialog flex flex-col justify-center items-center text-white absolute h-[220px] w-[320px] bottom-0 bg-black/50 ">
         <h2 className="Break w-[290px] h-[150px] mt-3 ml-2 overflow-auto">
           {props.text}
         </h2>
+        <div className=" flex p-7 w-[250px] h-10 items-center justify-around ">
+          <a href="" target="_blank" rel="noopener noreferrer">
+            <VscVmRunning className="h-7 w-7 cursor-pointer" />
+          </a>
+          <a href="http://" target="_blank" rel="noopener noreferrer">
+            <VscGithubInverted className="h-7 w-7 cursor-pointer" />
+          </a>
+        </div>
       </div>
     </div>
   );
