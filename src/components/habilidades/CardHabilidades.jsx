8b7a6af516553ba4c99c113fd2ff3bdde37a8954
Wellinton <wellintongerson@hotmail.com>
2023-10-09 22:44:14 -0300

    habilidades: ajuste na responsividade

--- src/components/habilidades/CardHabilidades.jsx
+++ src/components/habilidades/CardHabilidades.jsx
@@ -5,9 +5,14 @@ import { BiSolidCloudDownload } from "react-icons/bi";
 
 export function CardHabilidades(props) {
   return (
-    <li className="card p-10">
+    <li className="card p-10 w-[900px] h-[670px] md:w-[600px] md:h-[350px] sm:w-[410px] sm:h-[270px]">
       <div className="flip-card m-auto">
-        <img className="front object-cover" src={props.img} alt="" srcset="" />
+        <img
+          className="front object-cover md:object-contain sm:object-contain"
+          src={props.img}
+          alt=""
+          srcset=""
+        />
         <div className="back relative">
           <p className="absolute top-[40%] left-[50%] translate-x-[-50%] md:text-[11px] sm:text-[8px]">
             Clique para fazer o download
